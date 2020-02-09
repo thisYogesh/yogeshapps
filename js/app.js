6db2fdbeba831dc78181bd2628de4b7d6de88016
Yogesh Jagdale <yog99mail@gmail.com>
2020-02-09 15:58:06 +0530

    created mobile menu and css animation

--- js/app.js
+++ js/app.js
@@ -31,4 +31,25 @@ document.addEventListener('DOMContentLoaded', function(){
             previousSctollTop = scrollTop
         })
     }
+
+
+    const menuBtn = document.querySelector('#menu-btn');
+    if(menuBtn){
+        menuBtn.addEventListener('click', function(){
+            const isOpen = this.dataset.isMenuOpen
+            const menu = document.querySelector('#main-menu');
+            const menuIcon = document.querySelector('.menu-icon')
+
+            if(!isOpen || isOpen === 'close'){
+                menu.classList.add('--open');
+                menuIcon.classList.add('--menu-open');
+                this.dataset.isMenuOpen = 'open';
+            }else{
+                menu.classList.remove('--open');
+                menuIcon.classList.remove('--menu-open');
+                this.dataset.isMenuOpen = 'close';
+            }
+            
+        });
+    }
 })
