ddc5003d7f7586ac6bf5a0878b3691b48b524ac8
Yogesh Jagdale <yog99mail@gmail.com>
2020-02-15 22:29:35 +0530

    added scroll-snap-type

--- js/app.js
+++ js/app.js
@@ -1,43 +1,4 @@
 document.addEventListener('DOMContentLoaded', function(){
-    //
-    const mc = document.querySelector('.main-container')
-    if(mc){
-        const sectionList = mc.querySelectorAll('.main-section')
-        let previousSctollTop = 0
-        let aip = false
-        mc.dataset.currentSection = 0;
-        mc.addEventListener('scroll', function(e){
-            const scrollTop = this.scrollTop
-            if(aip){
-                previousSctollTop = scrollTop
-                return;
-            }else{
-                aip = true
-                setTimeout(function(){ aip = false }, 1000)
-            }
-
-            let cs = Number(this.dataset.currentSection)
-            let top = 0;
-            let update = false
-
-            if(scrollTop > previousSctollTop && sectionList[cs + 1]){
-                cs++;
-                update = true
-            }else if(sectionList[cs - 1]){
-                cs--;
-                update = true
-            }
-
-            if(update){
-                top = sectionList[cs].offsetTop
-                this.dataset.currentSection = cs;
-                this.scrollTo({ top: top, left: 0,behavior: 'smooth' })
-                previousSctollTop = scrollTop
-            }
-        })
-    }
-
-
     const menuBtn = document.querySelector('#menu-btn');
     if(menuBtn){
         menuBtn.addEventListener('click', function(){
