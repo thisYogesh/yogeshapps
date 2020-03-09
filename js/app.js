8033a3f18bf7864246810b8359e34b651b6b6764
Yogesh <yog99mail@gmail.com>
2020-03-09 19:55:09 +0530

    set scroll snap align on scroll

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
     const menuBtn = document.querySelector('#menu-btn');
+    const mc = document.querySelector('.main-container');
+
     if(menuBtn){
         menuBtn.addEventListener('click', function(){
             const isOpen = this.dataset.isMenuOpen
@@ -17,5 +19,39 @@ document.addEventListener('DOMContentLoaded', function(){
             }
             
         });
+    }
+
+    if(mc){
+        let st = 0
+        const sections = document.querySelectorAll('.main-section')
+        mc.addEventListener('scroll', function(e){
+            const scrollTop = this.scrollTop
+            const dir = this.dataset.direction;
+            let direction = ''
+            if(scrollTop > st){
+                direction = 'down'
+            }else{
+                direction = 'up'
+            }
+            
+            if(dir !== direction){
+                let scrollSnapAlign;
+                if(direction === 'down'){
+                    scrollSnapAlign = 'start'
+                    this.dataset.direction = 'down'
+                }else{
+                    scrollSnapAlign = 'end'
+                    this.dataset.direction = 'up'
+                }
+                
+                sections.forEach(function(section){
+                    section.style.scrollSnapAlign = scrollSnapAlign
+                })
+            }
+
+            // save previous scroll top value
+            st = scrollTop
+
+        })
     }
 })
